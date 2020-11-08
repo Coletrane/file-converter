b5192c1ff949e1abca5e620cd071432bf749f023
Cole <eloc49@gmail.com>
2020-11-08 10:55:01 -0500

    initial commit

---
+++ Builds/CLion/cmake-build-debug/CMakeFiles/APP.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_core.mm" "/Users/coleinman/Programs/c++/FileConverter/Builds/CLion/cmake-build-debug/CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_core.mm.o"
+  "/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_data_structures.mm" "/Users/coleinman/Programs/c++/FileConverter/Builds/CLion/cmake-build-debug/CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_data_structures.mm.o"
+  "/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_events.mm" "/Users/coleinman/Programs/c++/FileConverter/Builds/CLion/cmake-build-debug/CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_events.mm.o"
+  "/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_graphics.mm" "/Users/coleinman/Programs/c++/FileConverter/Builds/CLion/cmake-build-debug/CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_graphics.mm.o"
+  "/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_gui_basics.mm" "/Users/coleinman/Programs/c++/FileConverter/Builds/CLion/cmake-build-debug/CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_gui_basics.mm.o"
+  "/Users/coleinman/Programs/c++/FileConverter/Source/Main.cpp" "/Users/coleinman/Programs/c++/FileConverter/Builds/CLion/cmake-build-debug/CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/Source/Main.cpp.o"
+  "/Users/coleinman/Programs/c++/FileConverter/Source/MainComponent.cpp" "/Users/coleinman/Programs/c++/FileConverter/Builds/CLion/cmake-build-debug/CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/Source/MainComponent.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG=1"
+  "JUCER_XCODE_MAC_F6D2F4CF=1"
+  "JUCE_APP_VERSION=1.0.0"
+  "JUCE_APP_VERSION_HEX=0x10000"
+  "JUCE_DISPLAY_SPLASH_SCREEN=1"
+  "JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED=1"
+  "JUCE_MODULE_AVAILABLE_juce_core=1"
+  "JUCE_MODULE_AVAILABLE_juce_data_structures=1"
+  "JUCE_MODULE_AVAILABLE_juce_events=1"
+  "JUCE_MODULE_AVAILABLE_juce_graphics=1"
+  "JUCE_MODULE_AVAILABLE_juce_gui_basics=1"
+  "JUCE_PROJUCER_VERSION=0x60004"
+  "JUCE_STANDALONE_APPLICATION=1"
+  "JUCE_STRICT_REFCOUNTEDPOINTER=1"
+  "JUCE_USE_DARK_SPLASH_SCREEN=1"
+  "JucePlugin_Build_AAX=0"
+  "JucePlugin_Build_AU=0"
+  "JucePlugin_Build_AUv3=0"
+  "JucePlugin_Build_RTAS=0"
+  "JucePlugin_Build_Standalone=0"
+  "JucePlugin_Build_Unity=0"
+  "JucePlugin_Build_VST3=0"
+  "JucePlugin_Build_VST=0"
+  "_DEBUG=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../JuceLibraryCode"
+  "../../../../../../JUCE/modules"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Builds/CLion/cmake-build-debug/CMakeFiles/APP.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_core.mm.o"
+  "CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_data_structures.mm.o"
+  "CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_events.mm.o"
+  "CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_graphics.mm.o"
+  "CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/JuceLibraryCode/include_juce_gui_basics.mm.o"
+  "CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/Source/Main.cpp.o"
+  "CMakeFiles/APP.dir/Users/coleinman/Programs/c++/FileConverter/Source/MainComponent.cpp.o"
+  "NewProject.app/Contents/MacOS/NewProject"
+  "NewProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/APP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
